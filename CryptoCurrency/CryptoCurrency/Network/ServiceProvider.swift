aed2b619aebd8715cd683141f1992d0d582e46ff
Ana Carolina Martins Pessoa <anacarolinamartinspessoa@MacBook-Pro.local>
2022-02-14 15:32:25 -0300

    finish the network layer

--- CryptoCurrency/CryptoCurrency/Network/ServiceProvider.swift
+++ CryptoCurrency/CryptoCurrency/Network/ServiceProvider.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+struct CoinModel: Codable {
+    
+}
+struct ServiceProvider<T: Service> {
+    
+    private var executor: ServiceExecutor
+    var listner: ((ServiceState) -> Void)?
+    
+    init (executor: ServiceExecutor = Executor()) {
+        self.executor = executor
+    }
+    
+    func load(
+        service: T, decodeType: CoinModel.Type = CoinModel.self, deliverQueue: DispatchQueue = DispatchQueue.main, completion: ((Result<CoinModel, CustomError>) -> Void)?) {
+            executor.execute(service) { response in
+                switch response {
+                case .success (let data):
+                    if let resp = try? JSONDecoder().decode(CoinModel.self, from: data){
+                        deliverQueue.async {
+                            completion?(.success(resp))
+                        }
+                    }
+                case .failure(let error): deliverQueue.async {
+                    completion?(.failure(CustomError(error: error)))
+                }
+                }
+            }
+        }
+}
