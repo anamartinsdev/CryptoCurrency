c10afede5932b90ec83d10b881e615ce1eb12e6e
Ana Carolina Martins Pessoa <anacarolinamartinspessoa@MacBook-Pro.local>
2022-02-14 10:56:54 -0300

    implement CRUDE

--- CryptoCurrency/CryptoCurrency/ContentView.swift
+++ CryptoCurrency/CryptoCurrency/ContentView.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import MobileCoreServices
+
+struct Item: Identifiable{
+    let id = UUID()
+    let title: String
+}
 
 struct ContentView: View {
     //MARK: - Variables/Constants
+    @State private var items: [Item] = []
+    @State private var editMode = EditMode.inactive
+    private static var count = 0
     
     @Environment(\.colorScheme) var colorScheme
     
@@ -16,22 +25,50 @@ struct ContentView: View {
         NavigationView {
             VStack (alignment: .leading, spacing: 6) {
                 List {
-                    Text("Bitcoin").fontWeight(.bold).font(.system(size: 20))
-                    Text("Etherum").fontWeight(.bold).font(.system(size: 20))
-                    Text("Axie Infinity").fontWeight(.bold).font(.system(size: 20))
+                    ForEach(items) { item in
+                        Text(item.title)
+                    }
+                    .onDelete(perform: onDelete)
+                    .onMove(perform: onMove)
+                    .onInsert(of: [String(kUTTypeURL)], perform: onInsert)
                 }
                 .navigationBarTitle(Text("Crypto Currency"), displayMode: .automatic)
                 .navigationBarItems(leading: EditButton(), trailing: addButton)
                 .colorScheme(colorScheme)
+                .environment(\.editMode, $editMode)
             }
         }
     }
     private var addButton: some View {
         return AnyView(Button(action: onAdd) { Image(systemName: "plus") })
     }
+    
     private func onAdd() {
-        print("Button presed")
+        items.append (Item(title: "Item #\(Self.count)"))
+        Self.count += 1
     }
+    
+    private func onDelete(offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
+    
+    private func onMove (source: IndexSet, destination: Int) {
+        items.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    private func onInsert (at offset: Int, itemProvider: [NSItemProvider]) {
+        for provider in itemProvider {
+            if provider.canLoadObject(ofClass: URL.self) {
+                _ = provider.loadObject(ofClass: URL.self) { url, error in DispatchQueue.main.async {
+                    url.map { self.items.insert(Item(title: $0.absoluteString), at: offset) }
+                }
+                                                
+                                                }
+            }
+        }
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
