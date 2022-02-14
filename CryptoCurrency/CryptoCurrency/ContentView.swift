20e81947d37fa36a1e74efac9d6e2e7c28bf6d6b
Ana Carolina Martins Pessoa <anacarolinamartinspessoa@MacBook-Pro.local>
2022-02-14 09:34:07 -0300

    Create project

---
+++ CryptoCurrency/CryptoCurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCurrency
+//
+//  Created by Ana Carolina Martins Pessoa on 14/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoCurrency/CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Ana Carolina Martins Pessoa on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
