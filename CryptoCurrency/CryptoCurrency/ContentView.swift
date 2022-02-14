f2bf00e46ace326aad31573c1285ed2b5891d81f
Ana Carolina Martins Pessoa <anacarolinamartinspessoa@MacBook-Pro.local>
2022-02-14 09:55:59 -0300

    create list

--- CryptoCurrency/CryptoCurrency/ContentView.swift
+++ CryptoCurrency/CryptoCurrency/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - Variables/Constants
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack (alignment: .leading, spacing: 6) {
+                List {
+                    Text("Bitcoin").fontWeight(.bold).font(.system(size: 20))
+                    Text("Etherum").fontWeight(.bold).font(.system(size: 20))
+                    Text("Axie Infinity").fontWeight(.bold).font(.system(size: 20))
+                }
+                .navigationBarTitle(Text("Crypto Currency"), displayMode: .automatic)
+                .navigationBarItems(leading: EditButton(), trailing: addButton)
+                .colorScheme(colorScheme)
+            }
+        }
+    }
+    private var addButton: some View {
+        return AnyView(Button(action: onAdd) { Image(systemName: "plus") })
+    }
+    private func onAdd() {
+        print("Button presed")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(ColorScheme.allCases, id: \.self) {
+            ContentView().preferredColorScheme($0)
+        }
     }
 }
