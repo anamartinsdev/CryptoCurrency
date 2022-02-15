634c915cf4807128b02d2ea178f7867daf26edeb
Ana Carolina Martins Pessoa <anamartinspessoa@icloud.com>
2022-02-15 10:17:01 -0300

    create Coin Service

--- CryptoCurrency/CryptoCurrency/Services/CoinService.swift
+++ CryptoCurrency/CryptoCurrency/Services/CoinService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum CoinService {
+    case getCoins
+    case listAssets
+}
+
+extension CoinService: Service {
+    
+    var path: String {
+            return "/public/v1/coins"
+        }
+    
+    var method: ServiceMethod {
+            return .get
+        }
+    var parameters: [String : Any]? {
+        switch self {
+        case .getCoins:
+            return ["skip": "0", "limit":"10", "currency":"USD"]
+        case .listAssets:
+            return ["skip": "0", "limit":"25", "currency":"USD"]
+        }
+    }
+}
